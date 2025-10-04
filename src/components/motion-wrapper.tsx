f3cb9114eb823e2ed132c45616e6696578d574af
Zohaib Soomro <zohaibtop10@gmail.com>
2025-10-04 18:25:21 +0000

    make the navigations smoother

--- src/components/motion-wrapper.tsx
+++ src/components/motion-wrapper.tsx
@@ -3,16 +3,23 @@
 import { motion, AnimatePresence } from 'framer-motion';
 import { usePathname } from 'next/navigation';
 
+const variants = {
+    hidden: { opacity: 0, x: -5, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 5, y: 0 },
+};
+
 export function MotionWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   return (
-    <AnimatePresence mode="wait">
+    <AnimatePresence mode="wait" initial={false}>
       <motion.div
         key={pathname}
-        initial={{ opacity: 0, y: 10 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -10 }}
-        transition={{ duration: 0.2 }}
+        variants={variants}
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        transition={{ type: 'linear', duration: 0.15 }}
       >
         {children}
       </motion.div>
